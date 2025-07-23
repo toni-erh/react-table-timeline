e6d55faa97fe774368a66e4a15e352f7aafeae51
Toni Erhard <toni-erhard@freenet.de>
2025-07-23 21:19:55 +0200

    implemented incomplete data handling

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import { Table } from "./Table";
 
+const testRowCount = 20000
+
 const columns = ["index"];
-const data = new Array(20000).fill(true);
+const data = new Array(testRowCount).fill(true);
 
 export default function Playground() {
   const [rows] = useState(
@@ -13,7 +15,7 @@ export default function Playground() {
     <div style={{ padding: 32 }}>
       <h2>Table Playground</h2>
       <div style={{ padding: 16, border: "3px solid #ccc", borderRadius: 8, height: 400 }}>
-        <Table columns={columns} data={rows} />
+        <Table columns={columns} rows={rows} rowCount={testRowCount} />
       </div>
     </div>
   );
--- src/Table.tsx
+++ src/Table.tsx
@@ -1,37 +1,78 @@
 import React from "react";
 
 type TableRowBase = { index: number }
+type TableSceletonRow = { __sceleton_row: true }
+
+function isDataRow<TableRow extends TableRowBase>(row: TableRow | TableSceletonRow): row is TableRow {
+  return !(row as TableSceletonRow).__sceleton_row;
+}
 
 export interface TableProps<TableRow extends TableRowBase> {
   columns: string[];
-  data: Array<TableRow>;
+  rows: Array<TableRow>;
+  rowCount: number;
+  onRowRangeChange?: (firstRow: number, lastRow: number, rowCount: number) => void;
   lineHeight?: number;
+  rowVirtualizationMargin?: number;
 }
 
-export const Table = <TableRow extends TableRowBase = TableRowBase>({ columns, data, lineHeight = 20 }: TableProps<TableRow>) => {
+export const Table = <TableRow extends TableRowBase = TableRowBase>({ columns, rows, rowCount, onRowRangeChange, lineHeight = 20, rowVirtualizationMargin = 5 }: TableProps<TableRow>) => {
   const scrollContainerRef = React.useRef<HTMLDivElement>(null)
   const scrollBodyRef = React.useRef<HTMLDivElement>(null)
 
-  const [containerHeight, setContainerHeight] = React.useState<number>(0);
-  const rowCountToRender = React.useMemo(() => Math.ceil(containerHeight / lineHeight), [containerHeight])
+  const [firstRenderedIndex, setFirstRenderedIndex] = React.useState(0);
+  const [rowCountToRender, setRowCountToRender] = React.useState(0);
   React.useLayoutEffect(() => {
     const container = scrollContainerRef.current;
     if (container) {
-      setContainerHeight(container.clientHeight);
+      setRowCountToRender(Math.ceil(container.clientHeight / lineHeight) + rowVirtualizationMargin);
     }
   }, []);
-
-  const [firstRenderedIndex, setFirstRenderedIndex] = React.useState(0);
   React.useEffect(() => {
     const container = scrollContainerRef.current;
     if (!container) return;
-    const handleScroll = () => setFirstRenderedIndex(Math.floor(container.scrollTop / lineHeight));
+    const handleScroll = () => {
+      const virtualFirstIndex = Math.floor(container.scrollTop / lineHeight) - rowVirtualizationMargin
+      const firstIndex = Math.max(virtualFirstIndex, 0);
+      const lastIndex = Math.min(virtualFirstIndex + Math.ceil(container.clientHeight / lineHeight) + 2 * rowVirtualizationMargin, rowCount)
+      setFirstRenderedIndex(firstIndex)
+      setRowCountToRender(lastIndex - firstIndex)
+    }
     container.addEventListener('scroll', handleScroll);
 
     return () => {
       container.removeEventListener('scroll', handleScroll);
     }
   }, []);
+
+  React.useEffect(() => {
+    onRowRangeChange?.(firstRenderedIndex, firstRenderedIndex + rowCountToRender - 1, rowCountToRender)
+  }, [firstRenderedIndex, rowCountToRender])
+
+  const preparedRows: (TableRow | TableSceletonRow)[] = React.useMemo(() => {
+    if (rows.length === 0) return [];
+
+    const firstIndex = rows[0].index
+    if (firstIndex === undefined) throw Error(`All rows require an 'index'! It is missing for: ${JSON.stringify(rows[0])}`);
+
+    const relativeIndex = firstRenderedIndex - firstIndex
+
+    if (relativeIndex >= 0) {
+      const rowSection: (TableRow | TableSceletonRow)[] = rows.slice(relativeIndex, relativeIndex + rowCountToRender);
+      rowSection.push(...Array<TableSceletonRow>(rowCountToRender - rowSection.length).fill({ __sceleton_row: true }))
+      return rowSection
+    } else {
+      const sceletonCount = relativeIndex * -1
+      if (sceletonCount > rowCountToRender) {
+        return Array<TableSceletonRow>(rowCountToRender).fill({ __sceleton_row: true })
+      } else {
+        const rowSection = [...Array<TableSceletonRow>(sceletonCount).fill({ __sceleton_row: true }), ...rows.slice(0, rowCountToRender - sceletonCount)]
+        if (rowSection.length < rowCountToRender)
+          rowSection.push(...Array<TableSceletonRow>(rowCountToRender - rowSection.length).fill({ __sceleton_row: true }))
+        return rowSection
+      }
+    }
+  }, [rows, firstRenderedIndex, rowCountToRender])
 
   return (
     <div style={{ height: '100%' }}>
@@ -42,14 +83,20 @@ export const Table = <TableRow extends TableRowBase = TableRowBase>({ columns, d
       </div>
       <div ref={scrollContainerRef} style={{ overflow: 'scroll', height: `calc(100% - ${lineHeight}px)` }}>
 
-        <div ref={scrollBodyRef} style={{ height: `${(data.length - firstRenderedIndex) * lineHeight}px`, paddingTop: `${firstRenderedIndex * lineHeight}px` }}>
-          {data.slice(firstRenderedIndex, firstRenderedIndex + rowCountToRender).map((row) => (
-            <div key={row.index} style={{ height: `${lineHeight}px` }}>
-              {columns.map((col) => (
-                <div key={col}>{row[col as keyof TableRow] as React.ReactNode}</div>
-              ))}
-            </div>
-          ))}
+        <div ref={scrollBodyRef} style={{ height: `${(rowCount - firstRenderedIndex) * lineHeight}px`, paddingTop: `${firstRenderedIndex * lineHeight}px` }}>
+          {preparedRows.map((row, index) =>
+            isDataRow(row) ? (
+              <div key={row.index} style={{ height: `${lineHeight}px` }}>
+                {columns.map((col) => (
+                  <div key={col}>
+                    {col in row ? (row[col as keyof typeof row] as React.ReactNode) : null}
+                  </div>
+                ))}
+              </div>
+            ) : (
+              <div key={`sceleton_${index}`} style={{ height: `${lineHeight - 6}px`, marginBlock: '6px', backgroundColor: 'lightgrey', borderRadius: 5 }} />
+            )
+          )}
         </div>
       </div>
     </div>
