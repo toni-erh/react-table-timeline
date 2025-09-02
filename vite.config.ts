715bb5c79b71162546c1d32a1f0f07cd6650f546
Toni Erhard <toni-erhard@freenet.de>
2025-09-02 22:22:26 +0200

    fixed package building

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: "./",
+  build: {
+    lib: {
+      entry: resolve(__dirname, "src/Table.tsx"),
+      name: "ReactTableTimeline",
+      formats: ["es", "cjs"],
+      fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM"
+        }
+      }
+    },
+    sourcemap: true,
+    emptyOutDir: true
+  }
 });
