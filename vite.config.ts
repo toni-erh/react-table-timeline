4a5b7a821a8978b23074670f4a8e619716be3649
Toni Erhard <toni-erhard@freenet.de>
2025-09-03 11:57:08 +0200

    updated vite config to fix React 19 support

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,12 @@ export default defineConfig({
       fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ["react", "react-dom", "react/jsx-runtime"],
       output: {
         globals: {
           react: "React",
-          "react-dom": "ReactDOM"
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "ReactJsxRuntime"
         }
       }
     },
